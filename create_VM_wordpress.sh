8c906d9ef09bb85645ba264c9fe62142ae4795ea
Richard <richard-mank@hotmail.nl>
2025-04-02 18:44:08 +0200

    VM succesvol gemaakt in node pve02, scipt bijgewerkt.

--- create_VM_wordpress.sh
+++ create_VM_wordpress.sh
@@ -1,4 +1,19 @@
-# Stap 1: Maak een nieuwe VM aan
-sudo qm create 200 --name wpcrm --memory 2048 --cores 2 --net0 virtio,bridge=vmbr0 --ide2 local:cloudinit --ostype l26 --scsihw virtio-scsi-pci --scsi0 local-lvm:32
+# VM 200 aanmaken op HA-geschikte Ceph storage (vm-storage)
+sudo qm create 200 \
+  --name wpcrm \
+  --memory 2048 \
+  --cores 2 \
+  --net0 virtio,bridge=vmbr0 \
+  --scsihw virtio-scsi-pci \
+  --scsi0 vm-storage:32 \
+  --ide2 vm-storage:cloudinit \
+  --ostype l26
+
+# Boot volgorde en disk instellen
 sudo qm set 200 --boot c --bootdisk scsi0
+
+# Cloud-init config (voor testomgeving)
 sudo qm set 200 --ciuser wpadmin --cipassword securepass --ipconfig0 ip=10.24.13.200/24,gw=10.24.13.1
+
+# Voeg toe aan HA
+sudo ha-manager add vm:200
